05ccfabf989047cb1b54769e2d58cee1435c76e0
luckyXin <582519456@qq.com>
2013-11-11 15:21:53 +0800

    XXXX

---
+++ rudimentManageProject/src/main/java/dx/web/common/ReflectionObject.java
@@ -0,0 +1,16 @@
+package dx.web.common;
+
+public class ReflectionObject {
+
+	private ReflectionObject() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	private void systemOutPrint(){
+		System.out.println("已经输出");
+	}
+	
+	private int sum(int i, int n) {
+		return i + n;
+	}
+}
--- rudimentManageProject/src/main/java/dx/web/common/Test.java
+++ rudimentManageProject/src/main/java/dx/web/common/Test.java
@@ -1,10 +1,27 @@
 package dx.web.common;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IllegalArgumentException, InstantiationException, IllegalAccessException, InvocationTargetException {
 
+		Constructor<?> constructor = ReflectionObject.class.getDeclaredConstructors()[0];
+		constructor.setAccessible(true);
+		ReflectionObject reflectionObject = (ReflectionObject) constructor.newInstance();
+		
+		for(Method method : ReflectionObject.class.getDeclaredMethods()) {  
+			method.setAccessible(true);
+			if (method.getName().equals("systemOutPrint")) {
+				method.invoke(reflectionObject);     
+				
+			} else if (method.getName().equals("sum")){
+				int str = (Integer) method.invoke(reflectionObject, 2, 3);
+				System.out.println(str);
+			}
+		}
 	}
 
 }
