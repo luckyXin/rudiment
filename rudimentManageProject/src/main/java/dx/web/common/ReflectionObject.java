5aee3efbd58ec2c2037002d7c32ddfa1b0e5638e
luckyXin <582519456@qq.com>
2013-11-12 17:00:06 +0800

    XXX

--- rudimentManageProject/src/main/java/dx/web/common/ReflectionObject.java
+++ rudimentManageProject/src/main/java/dx/web/common/ReflectionObject.java
@@ -3,13 +3,12 @@ package dx.web.common;
 public class ReflectionObject {
 
 	private ReflectionObject() {
-		// TODO Auto-generated constructor stub
 	}
-	
-	private void systemOutPrint(){
+
+	private void systemOutPrint() {
 		System.out.println("已经输出");
 	}
-	
+
 	private int sum(int i, int n) {
 		return i + n;
 	}
--- rudimentManageProject/src/main/java/dx/web/common/Test.java
+++ rudimentManageProject/src/main/java/dx/web/common/Test.java
@@ -5,19 +5,21 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class Test {
-	public static void main(String[] args) throws IllegalArgumentException, 
-	InstantiationException, IllegalAccessException, InvocationTargetException {
+	public static void main(String[] args) throws IllegalArgumentException,
+			InstantiationException, IllegalAccessException,
+			InvocationTargetException {
 
-		Constructor<?> constructor = ReflectionObject.class.getDeclaredConstructors()[0];
+		Constructor<?> constructor = ReflectionObject.class
+				.getDeclaredConstructors()[0];
 		constructor.setAccessible(true);
-		ReflectionObject reflectionObject = (ReflectionObject) constructor.newInstance();
-		
-		for(Method method : ReflectionObject.class.getDeclaredMethods()) {  
+		ReflectionObject reflectionObject = (ReflectionObject) constructor
+				.newInstance();
+
+		for (Method method : ReflectionObject.class.getDeclaredMethods()) {
 			method.setAccessible(true);
 			if (method.getName().equals("systemOutPrint")) {
-				method.invoke(reflectionObject);     
-				
-			} else if (method.getName().equals("sum")){
+				method.invoke(reflectionObject);
+			} else if (method.getName().equals("sum")) {
 				int str = (Integer) method.invoke(reflectionObject, 2, 3);
 				System.out.println(str);
 			}
