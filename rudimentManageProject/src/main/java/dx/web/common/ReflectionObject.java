d008aebd80c8863f9989b2e254f9c77ae32d760e
luckyXin <582519456@qq.com>
2013-11-13 15:26:59 +0800

    pom 文件更新

--- rudimentManageProject/src/main/java/dx/web/common/ReflectionObject.java
+++ rudimentManageProject/src/main/java/dx/web/common/ReflectionObject.java
@@ -5,10 +5,12 @@ public class ReflectionObject {
 	private ReflectionObject() {
 	}
 
+	@SuppressWarnings("unused")
 	private void systemOutPrint() {
 		System.out.println("已经输出");
 	}
 
+	@SuppressWarnings("unused")
 	private int sum(int i, int n) {
 		return i + n;
 	}
