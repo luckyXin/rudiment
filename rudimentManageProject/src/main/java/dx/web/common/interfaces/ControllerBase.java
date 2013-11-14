9c72b1811fd56a88386fdc5f8a5a87c533de8377
luckyXin <582519456@qq.com>
2013-11-14 14:19:47 +0800

    框架搭建

--- rudimentManageProject/src/main/java/dx/web/common/interfaces/ControllerBase.java
+++ rudimentManageProject/src/main/java/dx/web/common/interfaces/ControllerBase.java
@@ -2,4 +2,5 @@ package dx.web.common.interfaces;
 
 public interface ControllerBase {
 
+	
 }
--- rudimentManageProject/src/main/java/dx/web/controller/UserInfoController.java
+++ rudimentManageProject/src/main/java/dx/web/controller/UserInfoController.java
@@ -1,5 +1,10 @@
 package dx.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
 public class UserInfoController {
 
 }
